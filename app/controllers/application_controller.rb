78765e13c24a074558a68a45d7a765cd69ed966b
LieutenantHawkeye <hwill12345@gmail.com>
2015-08-18 19:22:48 +0000

    Added forms and bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,29 @@ require "./config/environment"
 require "./app/models/tweet"
 
 class ApplicationController < Sinatra::Base
-  
+
   configure do
     set :public_folder, "public"
     set :views, "app/views"
   end
 
   get "/" do
-    h_tweet = Tweet.new("Lieutenant_Hawkeye", "Hello")
+    a_tweet = Tweet.new("Dev", "Hello")
+    b_tweet = Tweet.new("Dev", "This")
+    c_tweet = Tweet.new("Dev", "Is")
+    d_tweet = Tweet.new("Dev", "For")
+    e_tweet = Tweet.new("Dev", "Testing")
+    @all_tweets = Tweet.all
   	erb :index
   end
 
-
+  post "/new_tweet" do
+     puts params
+     Tweet.new(params[:username], params[:message])
+     @all_tweets = Tweet.all
+     puts Tweet.all
+     erb :index
+  end
 
 end
 
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,8 +1,14 @@
 class Tweet
   attr_accessor :username, :message
+  ALL_TWEETS = []
 
   def initialize(username, message)
   	@username = username
   	@message = message
+    ALL_TWEETS << self
+  end
 
+  def self.all
+    ALL_TWEETS
+  end
 end
